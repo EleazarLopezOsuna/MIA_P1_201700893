038869d8df8c6cee115169fcd83aa4f240fa7039
Eleazar Lopez <48371383+JaredOsuna@users.noreply.github.com>
2021-06-08 17:32:20 -0600

    Lista
    Lista para manejo de las particiones

---
+++ lista.h
@@ -0,0 +1,32 @@
+#ifndef LISTA_H
+#define LISTA_H
+#include <objetos.h>
+
+class lista{
+public:
+	lista();
+	struct nodoP{
+		char id[10];
+		char name[16];
+		struct nodoP *next;
+		struct nodoP *prev;
+	};
+	struct listaParticiones{
+		int size;
+		struct nodoP *first;
+		struct nodoP *last;
+	}
+	struct nodoC{
+		int character;
+		char path[255];
+		struct nodoC *next;
+		struct nodoC *prev;
+		struct listaParticiones *particiones;
+	}
+	struct list{
+		int size;
+		struct nodoC *primero;
+		struct nodoC *last;
+	}
+};
+#endif
